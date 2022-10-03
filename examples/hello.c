4373f04a41761976d67aa184f0633834cf436026
Hikaru Ikuta <woodrush924@gmail.com>
2022-10-03 21:55:45 +0900

    Print newline after message

--- examples/hello.c
+++ examples/hello.c
@@ -1,7 +1,7 @@
 int putchar(int c);
 
 int main (void) {
-    for (char* s = "Hello, world!"; *s; s++) {
+    for (char* s = "Hello, world!\n"; *s; s++) {
         putchar(*s);
     }
     return 0;
